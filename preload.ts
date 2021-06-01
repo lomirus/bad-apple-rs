5c70c5cf25d3d074ff27b810fe75cd93944e782c
Rhys <lomirus@qq.com>
2021-06-01 09:35:02 +0800

    dynamically get the data size info

--- preload.ts
+++ preload.ts
@@ -3,6 +3,10 @@ const decoder = new TextDecoder("utf-8");
 
 const binary = await Deno.readFile("src/source.txt");
 const source = decoder.decode(binary);
+
+getSize(source);
+const [FRAMES, ROWS, COLS] = getSize(source);
+
 let data = source
     .split("\n\n")
     .map(matrix => matrix.split("\n").map(row => `        ${row}`).join('],\n'))
@@ -10,6 +14,16 @@ let data = source
     .replaceAll("        ", "        [")
     .replaceAll("#", "true,  ")
     .replaceAll("-", "false, ");
-data = `pub const DATA: [[[bool; 101]; 44]; 4383] = [\n    [\n${data}]\n    ]\n];`
+data = `pub const DATA: [[[bool; ${COLS}]; ${ROWS}]; ${FRAMES}] = [\n    [\n${data}]\n    ]\n];`
 
 await Deno.writeFile("src/data.rs", encoder.encode(data));
+
+function getSize(source: string) {
+    const frames = source.split("\n\n");
+    const framesLength = frames.length;
+    const rows = frames[0].split('\n');
+    const rowsLength = rows.length;
+    const cols = rows[0];
+    const colsLength = cols.length;
+    return [framesLength, rowsLength, colsLength]
+}
--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,23 @@ use std::io::{stdout, Write};
 use std::thread::sleep;
 use std::time::Duration;
 
+const FRAMES: usize = data::DATA.len();
+const ROWS: usize = data::DATA[0].len();
+const COLS: usize = data::DATA[0][0].len();
+
 fn main() {
     let mut stdout = stdout();
-    for row in 0..44 {
+    for row in 0..ROWS {
         stdout.queue(MoveTo(27, row as u16)).unwrap();
-        for col in 0..101 {
+        for col in 0..COLS {
             stdout
                 .queue(Print(if data::DATA[0][row][col] { 'M' } else { ' ' }))
                 .unwrap();
         }
     }
-    for i in 1..4383 {
-        for row in 0..44 {
-            for col in 0..101 {
+    for i in 1..FRAMES {
+        for row in 0..ROWS {
+            for col in 0..COLS {
                 if data::DATA[i - 1][row][col] != data::DATA[i][row][col] {
                     stdout
                         .queue(MoveTo((col + 27) as u16, row as u16))
