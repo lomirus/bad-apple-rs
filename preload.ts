a9341a4bc826c9225d9fa7b631aa7f6ae70d1708
Rhys <lomirus@qq.com>
2021-06-01 08:54:32 +0800

    remove reduntant info in source

--- preload.ts
+++ preload.ts
@@ -1,12 +1,15 @@
 const encoder = new TextEncoder();
 const decoder = new TextDecoder("utf-8");
 
-const binary = await Deno.readFile("src/source.rs");
+const binary = await Deno.readFile("src/source.txt");
 const source = decoder.decode(binary);
-const data = source
+let data = source
+    .split("\n\n")
+    .map(matrix => matrix.split("\n").map(row => `        ${row}`).join('],\n'))
+    .join('],\n    ],\n    [\n')
     .replaceAll("        ", "        [")
-    .replaceAll("#\n", "true ],\n")
-    .replaceAll("-\n", "false],\n")
     .replaceAll("#", "true,  ")
-    .replaceAll("-", "false, ")
+    .replaceAll("-", "false, ");
+data = `pub const DATA: [[[bool; 101]; 44]; 4383] = [\n    [\n${data}]\n    ]\n];`
+
 await Deno.writeFile("src/data.rs", encoder.encode(data));
