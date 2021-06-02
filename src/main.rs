5aeb79136a0a2ae77ac5257259d893c50a0daebe
Rhys <lomirus@qq.com>
2021-06-02 21:27:55 +0800

    center display the animation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 mod data;
-use crossterm::{cursor::MoveTo, style::Print, QueueableCommand};
+use crossterm::{cursor::MoveTo, style::Print, QueueableCommand, terminal};
 use std::io::{stdout, Write};
 use std::thread::sleep;
 use std::time::Duration;
@@ -11,8 +11,9 @@ const FPS: u64 = 24;
 
 fn main() {
     let mut stdout = stdout();
+    let (left, top) = get_padding();
     for row in 0..HEIGHT {
-        stdout.queue(MoveTo(27, row as u16)).unwrap();
+        stdout.queue(MoveTo(left, top + row as u16)).unwrap();
         for col in 0..WIDTH {
             stdout
                 .queue(Print(if data::DATA[0][row][col] { 'M' } else { ' ' }))
@@ -24,7 +25,7 @@ fn main() {
             for col in 0..WIDTH {
                 if data::DATA[i - 1][row][col] != data::DATA[i][row][col] {
                     stdout
-                        .queue(MoveTo(col as u16 + 27, row as u16))
+                        .queue(MoveTo(left + col as u16 , top + row as u16))
                         .unwrap()
                         .queue(Print(if data::DATA[i][row][col] { 'M' } else { ' ' }))
                         .unwrap();
@@ -34,4 +35,19 @@ fn main() {
         stdout.flush().unwrap();
         sleep(Duration::from_millis(1000 / FPS));
     }
+}
+
+fn get_padding() -> (u16, u16) {
+    let (t_width, t_height) = terminal::size().unwrap();
+    let left = if WIDTH < t_width as usize {
+        (t_width - WIDTH as u16) / 2
+    } else {
+        0
+    };
+    let top = if HEIGHT < t_height as usize {
+        (t_height - HEIGHT as u16) / 2
+    } else {
+        0
+    };
+    (left, top)
 }
